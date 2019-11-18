d8b35df1ead56a04e8581002784ad3dd0c35382c
Ryan Couper <c3131273@uon.edu.au>
2019-11-18 11:46:01 +1100

    First pushed commit

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' # Tells Flask-Login what view function handles logging in (the name used in a url_for() call).
+                           # Used when a user attempts to access a page that requires the user to be logged in.
+
+from app import routes, models
