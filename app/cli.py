07cf698e3415a27c3bf614810a1a6fac63caa638
Ryan Couper <c3131273@uon.edu.au>
2019-12-03 09:25:44 +1100

    Added CLI commands for updating language translations, etc.

---
+++ app/cli.py
@@ -0,0 +1,36 @@
+from app import app
+import os
+import click
+
+@app.cli.group()
+def translate():
+    """Translation and localisation commands."""
+    pass
+
+
+@translate.command()
+def update():
+    """Update all languages."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('Extract command failed')
+    if os.system('pybabel update -i messages.pot -d app/translations'):
+        raise RuntimeError('Update command failed.')
+    os.remove('messages.pot')
+
+
+@translate.command()
+def compile():
+    """Compile all languages."""
+    if os.system('pybabel compile -d app/translations'):
+        raise RuntimeError('Compile command failed.')
+
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialise the new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('Extract command failed')
+    if os.system('pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('Init command failed')
+    os.remove('messages.pot')
